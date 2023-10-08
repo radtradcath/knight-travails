5bf28012c1879e25f3dfa585b7aa35c6222dff14
radtradcath <vinicius.b.bonafini@gmail.com>
2023-10-08 01:53:11 -0300

    Finish KT

--- script.js
+++ script.js
@@ -33,25 +33,6 @@ function isIncluded(array, value) {
   return false;
 }
 
-class Node {
-  constructor(position) {
-    this.vertex = position;
-    this.edges = [];
-  }
-}
-
-function buildGraph() {
-  let graph = [];
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      let node = new Node([i, j]);
-      node.edges = findEdges(node.vertex);
-      graph.push(node);
-    }
-  }
-  return graph;
-}
-
 class Knight {
   constructor() {}
 
@@ -59,11 +40,19 @@ class Knight {
     let queue = q; //[[0,0], 0]
     let sequence = visited; //[[0,0]];
     let initialEdges = findEdges(queue[0][0]); //[[1,2], [2,1]]
-    
-    while (queue.length > 0) {      
+
+    while (queue.length > 0) {
       let [position, moves] = queue[0];
       if (position[0] === final[0] && position[1] === final[1]) {
-        return position;
+        let untilFinal = sequence.slice(
+          0,
+          sequence.findIndex(
+            (element) => element[0] === final[0] && element[1] === final[1]
+          ) + 1
+        );
+        let path = reverseTree(position, sequence[0], untilFinal);
+        path.unshift(final);
+        return path;
       }
 
       for (let edge of initialEdges) {
@@ -73,13 +62,37 @@ class Knight {
         }
       }
       queue.shift();
-      console.log(sequence)
+
       this.knightMoves(position, final, queue, sequence);
     }
   }
 }
 
+function reverseTree(pos, initial, visited, arr = []) {
+  let moves = arr;
+  let posEdges = findEdges(pos);
+  if (pos[0] === initial[0] && pos[1] === initial[1]) {
+    return moves;
+  }
+  
+  for (let i = 0; i < posEdges.length; i++) {
+    if (isIncluded(visited, posEdges[i])) {
+      moves.push(posEdges[i]);
+      break;
+    }
+  }
+  let untilNext = visited.slice(
+    0,
+    visited.findIndex(
+      (element) =>
+        element[0] === moves[moves.length - 1][0] &&
+        element[1] === moves[moves.length - 1][1]
+    ) + 1
+  );
+
+  return reverseTree(moves[moves.length - 1], initial, untilNext, moves);
+}
+
 let knight = new Knight();
 
-console.log(knight.knightMoves([0, 0], [3,3]));
-console.log(knight.knightMoves([3, 3], [0,0]));
+console.log(knight.knightMoves([3, 4], [3, 2]));
