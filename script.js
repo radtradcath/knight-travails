90c0065aec4880c78e423b7fb4a26b6700c81f06
radtradcath <vinicius.b.bonafini@gmail.com>
2023-10-08 11:27:38 -0300

    Added comments to explain code parts

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Knight movement patterns
 let possibleMoves = [
   [2, 1],
   [-1, -2],
@@ -9,6 +10,7 @@ let possibleMoves = [
   [1, 2],
 ];
 
+// Get all the possible moves for a given possition
 function findEdges(vertex) {
   let edges = [];
   possibleMoves.forEach((move) => {
@@ -24,6 +26,7 @@ function findEdges(vertex) {
   return edges;
 }
 
+//Search if a given array is included in a 2D array
 function isIncluded(array, value) {
   for (let i = 0; i < array.length; i++) {
     if (value[0] === array[i][0] && value[1] === array[i][1]) {
@@ -33,50 +36,7 @@ function isIncluded(array, value) {
   return false;
 }
 
-class Knight {
-  constructor() {}
-
-  knightMoves(initial, final, q = [[initial, 0]], visited = [initial]) {
-    let queue = q; //[[0,0], 0]
-    let sequence = visited; //[[0,0]];
-    let initialEdges = findEdges(queue[0][0]); //[[1,2], [2,1]]
-
-    while (queue.length > 0) {
-      let [position, moves] = queue[0];
-      if (position[0] === final[0] && position[1] === final[1]) {
-        let untilFinal = sequence.slice(
-          0,
-          sequence.findIndex(
-            (element) => element[0] === final[0] && element[1] === final[1]
-          ) + 1
-        );
-        let path = reverseTree(position, sequence[0], untilFinal);
-        path.unshift(final);
-
-        let reversed = [];
-        for (let i = path.length - 1; i >= 0; i--) {
-          reversed.push(path[i]);
-        }
-        console.log(`You made it in ${moves} moves. Here's your path:`);
-        reversed.forEach((pos) => {
-          console.log(pos);
-        });
-        return;
-      }
-
-      for (let edge of initialEdges) {
-        if (!isIncluded(sequence, edge)) {
-          sequence.push(edge);
-          queue.push([edge, moves + 1]);
-        }
-      }
-      queue.shift();
-
-      return this.knightMoves(position, final, queue, sequence);
-    }
-  }
-}
-
+//Find the shortest path after BFS is done, using the visited nodes array
 function reverseTree(pos, initial, visited, arr = []) {
   let moves = arr;
   let posEdges = findEdges(pos);
@@ -100,6 +60,56 @@ function reverseTree(pos, initial, visited, arr = []) {
   );
 
   return reverseTree(moves[moves.length - 1], initial, untilNext, moves);
+}
+
+class Knight {
+  constructor() {}
+
+  knightMoves(initial, final, q = [[initial, 0]], visited = [initial]) {
+    let queue = q;
+    let sequence = visited;
+    let initialEdges = findEdges(queue[0][0]);
+
+    // Traverse through graph using BFS
+    while (queue.length > 0) {
+      let [position, moves] = queue[0];
+
+      // When final position is found, slice visited array from 0 to final position
+      if (position[0] === final[0] && position[1] === final[1]) {
+        let untilFinal = sequence.slice(
+          0,
+          sequence.findIndex(
+            (element) => element[0] === final[0] && element[1] === final[1]
+          ) + 1
+        );
+
+        // Call reverseTree to find the path that led to the end
+        let path = reverseTree(position, sequence[0], untilFinal);
+        path.unshift(final);
+
+        //Since path returns an array from last to first, reverse it for better display
+        let reversed = [];
+        for (let i = path.length - 1; i >= 0; i--) {
+          reversed.push(path[i]);
+        }
+        console.log(`You made it in ${moves} moves. Here's your path:`);
+        reversed.forEach((pos) => {
+          console.log(pos);
+        });
+        return;
+      }
+
+      for (let edge of initialEdges) {
+        if (!isIncluded(sequence, edge)) {
+          sequence.push(edge);
+          queue.push([edge, moves + 1]);
+        }
+      }
+      queue.shift();
+
+      return this.knightMoves(position, final, queue, sequence);
+    }
+  }
 }
 
 let knight = new Knight();
